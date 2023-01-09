71f9f25f55a014bbecd64af77f0bed3815a3fc20
LevKoganov <levk@playsmart.co.il>
2023-01-09 12:56:53 +0200

    updating schema field + name

---
+++ src/models/channel_sh.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import { IChannel } from "../../types";
+
+const reqString = {
+  type: String,
+  require: true,
+};
+
+const setChannel = new Schema<IChannel>({
+  guildId: reqString,
+  channelId: reqString,
+  type: reqString,
+  channelName: reqString,
+});
+
+export default model("Channel", setChannel);
