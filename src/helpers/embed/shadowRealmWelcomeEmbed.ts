742be9702aad6a25e1488d1d43c82cce22d1a302
LevKoganov <levk@playsmart.co.il>
2023-01-30 19:38:42 +0200

    new embed

---
+++ src/helpers/embed/shadowRealmWelcomeEmbed.ts
@@ -0,0 +1,11 @@
+import { EmbedBuilder } from "discord.js";
+
+export default (cooldown: string, imgPathString: string): EmbedBuilder => {
+  return new EmbedBuilder()
+    .setColor("Blurple")
+    .setTitle("Shadow realm")
+    .setDescription(
+      `**You may escape the \`shadow realm\` after \`1 hours\` since the moment your been banished.**\n\n\`\`\`time passed: ${cooldown} (HH:mm:ss)\`\`\``
+    )
+    .setImage(`attachment://${imgPathString}`);
+};
