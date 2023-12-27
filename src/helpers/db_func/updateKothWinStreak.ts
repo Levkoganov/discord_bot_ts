99c07dd7fd86e4d13605979b187b17edfa90a3d5
LevKoganov <levk@playsmart.co.il>
2023-12-27 17:26:23 +0200

    return winner data

--- src/helpers/db_func/updateKothWinStreak.ts
+++ src/helpers/db_func/updateKothWinStreak.ts
@@ -1,12 +1,14 @@
 import { ISetChampion } from "../../../types";
 import champion_sh from "../../models/champion_sh";
 
-export default async (winner: ISetChampion | null): Promise<void> => {
+export default async (winner: ISetChampion | null) => {
   if (winner) {
-    await champion_sh.findOneAndUpdate(
+    const winnersData = await champion_sh.findOneAndUpdate(
       { _id: winner._id },
       { $inc: { winstreak: 1 } },
       { new: true, upsert: true, useFindAndModify: false }
     );
+
+    return winnersData as ISetChampion;
   }
 };
